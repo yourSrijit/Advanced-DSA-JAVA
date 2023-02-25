07046eeaa82123c94b49fd0a2684cfa022fc1e4c
Srijit Bera <berasrijit02@gmail.com>
2023-02-25 23:29:36 +0530

    Update clear_RangeofBits.java

--- Bit Manipulation in Java/clear_RangeofBits.java
+++ Bit Manipulation in Java/clear_RangeofBits.java
@@ -1,7 +1,13 @@
 package BitManipulation;
 
 public class clear_RangeofBits {
+    public static void clearRange(int n,int i,int k){
+        int a =(~0)<<(k+1);
+        int b=(1<<i)-1;
+        int bitMask =a | b;
+        System.out.println("After change the result is "+(bitMask & n));
+    }
     public static void main(String[] args) {
-        
+    clearRange(10,2,4);
     }
 }
