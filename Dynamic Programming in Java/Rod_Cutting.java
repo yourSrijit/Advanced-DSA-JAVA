8b097ad8052a8964df3f74ba5b2b37dbdff9c11b
Srijit Bera <berasrijit02@gmail.com>
2023-11-15 10:11:20 +0530

    Add files via upload

---
+++ Dynamic Programming in Java/Rod_Cutting.java
@@ -0,0 +1,29 @@
+package Dynamic_Programming;
+
+public class Rod_Cutting {
+    public static int roadCutting(int length[], int price[], int totalRoad) {
+        int n = price.length;
+        int m = totalRoad;
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        int length[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
+        int totalRoad = 8;
+        System.out.println(roadCutting(length, price, totalRoad));
+    }
+}
